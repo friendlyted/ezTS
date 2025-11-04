1d868ce6a493e81e38f3f550186f9a81ebe913e7
friendlyted <tubist.fedor@gmail.com>
2025-11-04 11:50:44 +0300

    reorg files, removed old samples

--- showcase/simple/starter.js
+++ showcase/simple/starter.js
@@ -1,6 +1,5 @@
 import {importTS, installSW} from "../../loader/front.js";
-
 await installSW("sw.js");
 
-const mainModule = await importTS("../simple/main_module.ts");
+const mainModule = await importTS("ts/main_module.ts");
 mainModule.main();
